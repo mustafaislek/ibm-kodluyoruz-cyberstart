7988b07fc92a66b2a9f07a5a12783c3170db8fe1
Mustafa İşlek <33036930+mustafaislek@users.noreply.github.com>
2024-09-19 10:43:45 +0300

    Add assignment

--- min_euclidean_distance.py
+++ min_euclidean_distance.py
@@ -1,1 +1,16 @@
+import math
 
+points = [(3, 4), (7, 9), (1, 6), (8, 3)]
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
+distances = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        dist = euclideanDistance(points[i], points[j])
+        distances.append(dist)
+
+min_distance = min(distances)
+print(f"Minimum Distance: {min_distance}")
